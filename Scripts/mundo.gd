4a6a7607e7b62c89ac36b3e0e24057f0fade9b53
Andrey Cesar <andreycesarrodrigues@hotmail.com>
2021-08-29 08:15:43 -0300

    Refatorando a animação

--- Scripts/mundo.gd
+++ Scripts/mundo.gd
@@ -14,6 +14,15 @@ onready var nuvem2    : Node = $cenario/nuvem2;
 #Var responsavel por controlar o tempo de crianção da coluna
 var tempo_instanciar : float = 0;
 
+func mover_cenario(node1:Node, node2:Node, velocidade:float, posicao:float) -> void:
+	node1.position.x -= velocidade;
+	node2.position.x -= velocidade;
+	
+	if node1.position.x <= -posicao:
+		node1.position.x = posicao;
+	if node2.position.x <= -posicao:
+		node2.position.x = posicao;
+
 func criar_colunas():
 	
 	#Criando uma instância de coluna
@@ -37,33 +46,12 @@ func _process(delta) -> void:
 		criar_colunas();
 		tempo_instanciar = 0;
 	
-	#Movendo o fundo
-	fundo1.position.x -= 0.5;
-	fundo2.position.x -= 0.5;
-	
-	#Resetando a posição do fundo ao chegar na posição -1085
-	if fundo1.position.x <= -1085:
-		fundo1.position.x = 1085;
-	if fundo2.position.x <= -1085:
-		fundo2.position.x = 1085;
-	
-	montanha1.position.x -= 3;
-	montanha2.position.x -= 3;
-	
-	if montanha1.position.x <= -1085:
-		montanha1.position.x = 1085;
-	if montanha2.position.x <= -1085:
-		montanha2.position.x = 1085;
-	
-	nuvem1.position.x -= 9;
-	nuvem2.position.x -= 9;
-	
-	if nuvem1.position.x <= -2160:
-		nuvem1.position.x = 2160;
-	if nuvem2.position.x <= -2160:
-		nuvem2.position.x = 2160;
-	
-	
+	#Movendo a nuvem
+	mover_cenario(nuvem1,nuvem2,9,2160);
+	#Movendo a montanha
+	mover_cenario(montanha1, montanha2,4, 1082);
+	#movendo fundo
+	mover_cenario(fundo1, fundo2, 0.5, 1085);
 
 
 
