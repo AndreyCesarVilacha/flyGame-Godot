1b0f67cd13117cadcc24bad5d860ceaceaa6e615
Andrey Cesar <andreycesarrodrigues@hotmail.com>
2021-08-29 07:57:11 -0300

    Animação nuvens e montanha

--- Scripts/mundo.gd
+++ Scripts/mundo.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
 #Obtendo o caminho da Scene Coluna
-const pre_colunas : Resource = preload("res://Scenes/Colunas.tscn");
+const pre_colunas     : Resource = preload("res://Scenes/Colunas.tscn");
 #Obtendo os fundos
-onready var fundo1 : Node = get_node("cenario/fundo");
-onready var fundo2 : Node = $cenario/fundo2;
+onready var fundo1    : Node = get_node("cenario/fundo");
+onready var fundo2    : Node = $cenario/fundo2;
+onready var montanha1 : Node = $cenario/montanha;
+onready var montanha2 : Node = $cenario/montanha2;
+onready var nuvem1    : Node = $cenario/nuvem;
+onready var nuvem2    : Node = $cenario/nuvem2;
 
 
 #Var responsavel por controlar o tempo de crianção da coluna
@@ -42,6 +46,24 @@ func _process(delta) -> void:
 		fundo1.position.x = 1085;
 	if fundo2.position.x <= -1085:
 		fundo2.position.x = 1085;
+	
+	montanha1.position.x -= 3;
+	montanha2.position.x -= 3;
+	
+	if montanha1.position.x <= -1085:
+		montanha1.position.x = 1085;
+	if montanha2.position.x <= -1085:
+		montanha2.position.x = 1085;
+	
+	nuvem1.position.x -= 9;
+	nuvem2.position.x -= 9;
+	
+	if nuvem1.position.x <= -2160:
+		nuvem1.position.x = 2160;
+	if nuvem2.position.x <= -2160:
+		nuvem2.position.x = 2160;
+	
+	
 
 
 
